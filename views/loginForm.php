ed2053ef3e0c91b9d055b8e47413bb2958b37f07
Harold Sikkema <harold@nsitu.ca>
2021-12-20 14:55:15 -0500

    use app root for links

--- views/loginForm.php
+++ views/loginForm.php
@@ -12,12 +12,12 @@
         <div class="col col-sm-6 col-md-6 col-lg-4 col-xl-3">
           <!-- note the form's action means that is is submitting to /login 
           You can check the /login route to see what happens next. -->
-            <form  method="post" action="/login">
+            <form  method="post" action="<?= App::root(); ?>/login">
               <label class="text-muted">Your credentials, please.</label>
               <input class="form-control form-control-lg my-1" type="email" name="Email" placeholder="Email Address" required>
               <input class="form-control form-control-lg my-1" type="password" name="Password" placeholder="Password" required>
               <button class="btn btn-info btn-lg btn-block my-3" type="submit" name="Login" value="Login">Login</button>
-              <p class="text-muted text-center">Need an Account? <a href="/register">Register Here</a>.</p>
+              <p class="text-muted text-center">Need an Account? <a href="<?= App::root(); ?>/register">Register Here</a>.</p>
             </form>
         </div>
     </div>
