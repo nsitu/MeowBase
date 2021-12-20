5083a8372de1daa8d0190a40502c615aa9f45989
Harold Sikkema <harold@nsitu.ca>
2021-12-20 15:30:33 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
+
+// During development, show all errors. 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
   
 // Autoload Classes 
 spl_autoload_register(function($class_name){
