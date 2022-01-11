59e6348174405fc9d273befda97ab95e23db5f53
Harold Sikkema <harold@nsitu.ca>
2022-01-10 22:26:02 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 
-// During development, show all errors. 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// During development, uncomment to show  errors. 
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
   
 // Autoload Classes 
